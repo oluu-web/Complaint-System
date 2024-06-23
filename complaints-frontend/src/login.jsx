b14e691f1dd6e386fe0175d9dbbbd320693b98b8
Olumide Akinkunmi <mide.akinkunmi@gmail.com>
2024-06-24 00:38:16 +0100

    I lied.

--- complaints-frontend/src/login.jsx
+++ complaints-frontend/src/login.jsx
@@ -44,10 +44,13 @@ const Login = () => {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100 py-12 px-4 sm:px-6 lg:px-8">
-      <div className="max-w-md w-full space-y-8">
-        <div>
-          <h2 className="mt-6 text-center text-2xl font-extrabold text-gray-900">CU Revalidation System</h2>
+    <div className="min-h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8" style={{
+      "backgroundImage" : `url("https://independent.ng/wp-content/uploads/Drone-view-of-Covenanat-University-Ota-Ogun-State-727x430.jpg")`,
+      "backgroundSize": 'cover',
+      }}>
+      <div className="max-w-md w-full space-y-8 bg-white">
+        <div className='flex justify-center'>
+          <img src='https://cuportal.covenantuniversity.edu.ng/assets/img/CU_LOGO.jpg' alt= 'CU Logo' height={"100px"} width={"100px"} className='mt-5'></img>
         </div>
         <div>
           <h3 className="mt-6 text-center text-2xl font-bold text-gray-900">Sign in to your account</h3>
